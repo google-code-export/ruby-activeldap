707740f0b68544038f0af69a6e5efd1d2ec27bdc
koutou <koutou@fd4f1387-ac19-0410-9179-05984c98faae>
2009-07-17 23:02:19 +0000

    New release tag

---
+++ tags/r1.1.0/lib/active_ldap/association/belongs_to_many.rb
@@ -0,0 +1,50 @@
+require 'active_ldap/association/collection'
+
+module ActiveLdap
+  module Association
+    class BelongsToMany < Collection
+      private
+      def insert_entry(entry)
+        old_value = entry[@options[:many], true]
+        primary_key_name = @options[:primary_key_name]
+        if primary_key_name == "dn"
+          old_value = dn_values_to_string_values(old_value)
+        end
+        new_value = old_value + @owner[primary_key_name, true]
+        new_value = new_value.uniq.sort
+        if old_value != new_value
+          entry[@options[:many]] = new_value
+          entry.save
+        end
+      end
+
+      def delete_entries(entries)
+        entries.each do |entry|
+          old_value = entry[@options[:many], true]
+          primary_key_name = @options[:primary_key_name]
+          if primary_key_name == "dn"
+            old_value = dn_values_to_string_values(old_value)
+          end
+          new_value = old_value - @owner[primary_key_name, true]
+          new_value = new_value.uniq.sort
+          if old_value != new_value
+            entry[@options[:many]] = new_value
+            entry.save
+          end
+        end
+      end
+
+      def find_target
+        values = @owner[@options[:primary_key_name], true].compact
+        return [] if values.empty?
+
+        key = @options[:many]
+        components = values.collect do |value|
+          [key, value]
+        end
+        options = find_options(:filter => [:or, *components])
+        foreign_class.find(:all, options)
+      end
+    end
+  end
+end
